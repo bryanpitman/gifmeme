b8726e114d8e28cd28a23e35b885f611f629a146
Bryan Pitman <pitman.bryan@gmail.com>
2022-07-19 15:47:33 -0700

    added styles and mod index

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+"use strict";
 console.log("Let's get this party started!");
+
+
+function showGifs(res) {
+  $("div").html(res);
+}
+
+async function getGiffy() {
+
+  let search = $("inputText").val();
+  let response = await axios.post("api.giphy.com/v1/gifs/search", { search });
+}
+
+function search(evt) {
+  $("#search").on("click", getGiffy);
+  event.preventDefault(evt);
+}
+search();
