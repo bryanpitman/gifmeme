7248b190e0e2620103bea38f0d06fd6541ef6d01
Billyhrussell <billy.hrussell@gmail.com>
2022-07-19 17:38:06 -0700

    imgs show up

--- app.js
+++ app.js
@@ -6,16 +6,30 @@ function showGifs(res) {
   $("div").html(res);
 }
 
+/**
+ * Get the input text and in response, send get request to API giphy, returns giphy obj
+ * setting url to link in giphy obj
+ * append img to div
+ */
 async function getGiffy() {
 
-  let $search = $("inputText").val();
-  console.log($search);
+  let search = $("#inputText").val();
+  console.log(search);
   let key = "KvIxjZLWisiNgR92hpRiYpVTyZStjjKq";
-  let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q:$search, api_key:key}});
-  // console.log(response.data.data[0].url);
-  console.log(response);
+  let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q:search, api_key:key}});
+  let url = response.data.data[0].images.original.url;
+
+  $("div").append(`<img src = "${url}">`);
+
+  //$(<img></img>)
+
+  //console.log(response);
 }
 
+/**
+ * when submit button clicked, run getGiffy()
+ *
+ */
 function search(event) {
   // $("#search").on("click", getGiffy(event){
   //   event.preventDefault();
